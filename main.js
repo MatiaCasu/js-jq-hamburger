9cf760710855184e776abc8e7893ada43b85b0cc
Matia <matia.casu@gmail.com>
2021-03-11 15:58:00 +0100

    Aggiunta chiusura al click di close

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ mostrare / nascondere il menu principale
 
 var menuBars = $(".header-right a i.fa-bars");
 var menuActive = $(".hamburger-menu");
-
+var menuClose = $(".close");
 
 // Creata variabile menuBars con evento click
 menuBars.click(
   function(){
       menuActive.addClass("active");
+  }
+);
+
+menuClose.click(
+  function(){
+    menuActive.removeClass("active");
   }
 );
